c94222b9e209b61a56fd78ac8829fd3d0b611ab6
Jatin Dhankhar <dhankhar.jatin@gmail.com>
2016-10-17 12:47:45 +0530

    Structure app

--- app/src/paid/java/com/udacity/gradle/builditbigger/paid/MainActivityFragment.java
+++ app/src/paid/java/com/udacity/gradle/builditbigger/paid/MainActivityFragment.java
@@ -1,22 +1,20 @@
-package com.udacity.gradle.builditbigger;
+package com.udacity.gradle.builditbigger.paid;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
-import com.google.android.gms.ads.InterstitialAd;
-
+import com.udacity.gradle.builditbigger.MainActivity;
+import com.udacity.gradle.builditbigger.R;
 
 /**
- * A placeholder fragment containing a simple view.
+ * Created by jatin on 10/17/16.
  */
+
 public class MainActivityFragment extends Fragment {
-    InterstitialAd mInterstitialAd;
     public MainActivityFragment() {
     }
 
